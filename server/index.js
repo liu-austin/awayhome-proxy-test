d9a699612f6dc8cdeedcc43da49a4b87cb5458bd
Katherine Wang <katherine.wang429@gmail.com>
2019-12-11 12:31:58 -0800

    Test proxy init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
