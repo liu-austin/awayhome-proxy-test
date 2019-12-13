4b64a6247d8085f8209014bc5a569513617b369d
liu-austin <austinliu279@gmail.com>
2019-12-12 17:56:41 -0800

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './client/src'),
+  entry: {
+      austin: path.resolve(__dirname, './search-bar-booking-tool/client/src'),
+      kat: path.resolve(__dirname, './overview-amenities/client/src'),
+      matthew: path.resolve(__dirname, './Reviews/client/src')
+  },
   output: {
-    path: path.resolve(__dirname, './client/dist'),
-    filename: 'bundle.js',
+      path: path.resolve(__dirname, './client/dist'),
+      filename: "[name]-bundle.js"
   },
   mode: 'development',
   module: {
     rules: [
       {
-        loader: 'babel-loader',
         test: /\.js[x]?/,
         exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env', {'plugins': ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime']}]
+          }
+        }
       },
+      {
+        test: /\.(scss|css)$/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
     ],
   },
   resolve: {
